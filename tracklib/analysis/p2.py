25f99f390c47608924f3c7669e83496aef96d03f
Simon Grosse-Holz <sgh256@mit.edu>
2022-07-23 05:19:42 -0400

    removed useless warning in p2.py:171/173

--- tracklib/analysis/p2.py
+++ tracklib/analysis/p2.py
@@ -168,9 +168,9 @@ def P2dataset(dataset, givevar=False, giveN=False, average_in_logspace=False, **
         allN = allN[:, 1:]
 
     N = np.sum(allN, axis=0)
-    meanN = N / np.sum(allN != 0, axis=0)
     with warnings.catch_warnings():
         warnings.filterwarnings(action='ignore', message=r'(invalid value|divide by zero) encountered in true_divide')
+        meanN = N / np.sum(allN != 0, axis=0)
         eP2 = np.nansum(allP2*allN, axis=0) / N
         if givevar:
             var = np.nansum((allP2-eP2)**2 * allN, axis=0) / (N-meanN)
